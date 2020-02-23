7fcbe00295218368d0c5287907dd9675385da86e
Renat <rinoz1@yandex.ru>
2020-02-23 17:25:09 +0500

    create util and mapper tests

--- project/app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/MapperTest.kt
+++ project/app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/MapperTest.kt
@@ -1,4 +1,15 @@
 package ru.steeloscar.gitinfocleanarchitecture
 
+import junit.framework.Assert.*
+import io.reactivex.Observable
+import org.junit.Test
+import ru.steeloscar.gitinfocleanarchitecture.data.repository.api.model.RepositoryCommit
+
 class MapperTest {
+    @Test
+    fun arrayListRepositoryCommitToEntity() {
+        val list = Observable.just(ArrayList<RepositoryCommit>())
+        val expectedList = Observable.just(ArrayList<RepositoryCommit>())
+        assertEquals(expectedList, list)
+    }
 }
--- project/app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/UtilTest.kt
+++ project/app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/UtilTest.kt
@@ -1,4 +1,20 @@
 package ru.steeloscar.gitinfocleanarchitecture
 
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import ru.steeloscar.gitinfocleanarchitecture.commons.formatDate
+import ru.steeloscar.gitinfocleanarchitecture.commons.formatSha
+
 class UtilTest {
+    @Test
+    fun testFormatDate(){
+        val date = formatDate("1234-56-78T90:12:34Z")
+        assertEquals("78.56.1234", date)
+    }
+
+    @Test
+    fun testFormatSha() {
+        val sha = formatSha("123456789qwerty123456789qwerty1234567890")
+        assertEquals("1234567", sha)
+    }
 }

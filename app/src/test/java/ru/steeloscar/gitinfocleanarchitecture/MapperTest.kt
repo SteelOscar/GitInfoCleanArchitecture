5a725b801ed407d7d9e4b228defc79b9e256ff58
Renat <rinoz1@yandex.ru>
2020-02-23 20:08:47 +0500

    added 3 more methods but didn't finish them

--- app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/MapperTest.kt
+++ app/src/test/java/ru/steeloscar/gitinfocleanarchitecture/MapperTest.kt
@@ -1,15 +1,36 @@
 package ru.steeloscar.gitinfocleanarchitecture
 
-import junit.framework.Assert.*
-import io.reactivex.Observable
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import ru.steeloscar.gitinfocleanarchitecture.data.mapper.ArrayListUserProfileEntityMapper
 import ru.steeloscar.gitinfocleanarchitecture.data.repository.api.model.RepositoryCommit
+import ru.steeloscar.gitinfocleanarchitecture.data.repository.api.model.UserRepository
+import ru.steeloscar.gitinfocleanarchitecture.datasource.TestDataSourceUserProfile
+import ru.steeloscar.gitinfocleanarchitecture.domain.entity.RepositoryCommitEntity
+import ru.steeloscar.gitinfocleanarchitecture.domain.entity.UserRepositoryEntity
 
 class MapperTest {
     @Test
+    fun arrayListUserProfileToEntity() {
+        val realList = ArrayListUserProfileEntityMapper.map(TestDataSourceUserProfile.getUsersProfile())
+        val expectedList = TestDataSourceUserProfile.getUsersProfileEntity()
+        assertEquals(expectedList::class.java, realList::class.java)
+    }
+
+    @Test
+    fun arrayListUserRepositoryToEntity() {
+        val list = ArrayList<UserRepository>()
+        val expectedList = ArrayList<UserRepositoryEntity>()
+    }
+
+    @Test
     fun arrayListRepositoryCommitToEntity() {
-        val list = Observable.just(ArrayList<RepositoryCommit>())
-        val expectedList = Observable.just(ArrayList<RepositoryCommit>())
-        assertEquals(expectedList, list)
+        val list = ArrayList<RepositoryCommit>()
+        val expectedList = ArrayList<RepositoryCommitEntity>()
+    }
+
+    @Test
+    fun updateUserProfileBodyToEntityMapper() {
+
     }
 }
